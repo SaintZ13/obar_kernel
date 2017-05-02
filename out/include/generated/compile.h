d69c9063b015193eea6149e66a3b1b0a8e980b39
SaintZ13 <calmdownkid13@gmail.com>
2017-05-02 14:49:06 -0400

    Add missing analogix file

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Tue May 2 14:24:55 EDT 2017"
+#define LINUX_COMPILE_BY "emotion"
+#define LINUX_COMPILE_HOST "emotion-os"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (Linaro GCC 4.9-2015.06) "
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.31.OBAR.Kernel.vR1+"
