77740978da578cd24713d4ceda29dd04ef4aa728
SaintZ13 <calmdownkid13@gmail.com>
2017-05-03 01:34:09 -0400

    revert drivers: wakeup: there's no much point in running the blockers during screen on

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Wed May 3 01:19:23 EDT 2017"
+#define LINUX_COMPILE_BY "emotion"
+#define LINUX_COMPILE_HOST "emotion-os"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (Linaro GCC 4.9-2015.06) "
