642ac6a7cbbf7c0dcf7cbf16153d39c5550556fd
SaintZ13 <calmdownkid13@gmail.com>
2017-06-07 00:48:05 -0400

    Remove build and out folders from source

--- build/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Wed Jun 7 00:28:47 EDT 2017"
-#define LINUX_COMPILE_BY "SaintZ93"
-#define LINUX_COMPILE_HOST "hidden"
-#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (Linaro GCC 4.9-2015.06) "
